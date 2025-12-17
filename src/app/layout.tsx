39676244e7ff4c6967cd6cbe02ae699696600548
Javed Hussain <jvdhussain06@gmail.com>
2025-12-17 11:42:34 +0000

    make it pwa and can be install okay make it proper instaleb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className={cn(
           "font-body antialiased h-full",
