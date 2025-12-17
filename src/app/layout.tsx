da277c9f6423a84fed49924532207f71708c1e28
Javed Hussain <jvdhussain06@gmail.com>
2025-12-17 12:08:58 +0000

    okay so here is the code update it in there files okay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3385280084392007"
+     crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
